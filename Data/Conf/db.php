c5282285ca381d8a83d69ee68f240f4ab272fee3
hedong <dong.he01@billionsfinance.cn>
2017-01-20 16:06:48 +0800

    db

--- Data/Conf/db.php
+++ Data/Conf/db.php
@@ -2,11 +2,11 @@
 return array(
     /* 数据库配置 */
 	'DB_TYPE' => 'mysql',
-	'DB_HOST'=>'',
-	'DB_USER'=>'',
-	'DB_PWD'=>'',
+	'DB_HOST'=>'112.74.172.145',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'Y1983326x',
 	'DB_PORT'=>3306,
-	'DB_NAME'=>'',
+	'DB_NAME'=>'wemall',
 	'DB_CHARSET' => 'utf8',
 	'DB_PREFIX'=>'multi_',
 );
